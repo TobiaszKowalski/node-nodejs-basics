966ce1372c0e02861cce5cad1d38fe78d57eacaa
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-17 15:46:29 +0200

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fresh.txt');
+  const fileContent = "I am fresh and young";
+  const fsFailedErrorMessage = "FS operation failed";
+
+  try {
+    await fs.promises.access(filePath, fs.constants.F_OK);
+    throw new Error(fsFailedErrorMessage);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await fs.promises.writeFile(filePath, fileContent);
+    } else {
+      console.error(error);
+    }
+  }
 };
 
 await create();

582e21b7925902491fe38f62a39a2929073ed375
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-17 23:17:34 +0200

    feat: implement delete function

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,18 @@
+import fs from "fs";
+import path from "path";
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const fileToRemovePath = path.join(__dirname, "files", "fileToRemove.txt");
+  const fsFailedErrorMessage = "FS operation failed";
+
+  try {
+    await fs.promises.access(fileToRemovePath, fs.constants.F_OK);
+    await fs.promises.unlink(fileToRemovePath);
+  } catch (error) {
+    throw new Error(fsFailedErrorMessage);
+  }
 };
 
 await remove();

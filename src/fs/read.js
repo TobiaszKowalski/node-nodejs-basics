53027f1c83d4b812f5af8a7c4b4d9bfab46ab97f
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-17 23:46:31 +0200

    feat: implement read function

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,17 @@
+import { readFile } from "fs/promises";
+import { dirname, join } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const fileToReadPath = join(__dirname, "files", "fileToRead.txt");
+  const fsFailedErrorMessage = "FS operation failed";
+
+  try {
+    await readFile(fileToReadPath, "utf-8").then((fileContent) => console.log(fileContent));
+  } catch (error) {
+    throw new Error(fsFailedErrorMessage);
+  }
 };
 
 await read();

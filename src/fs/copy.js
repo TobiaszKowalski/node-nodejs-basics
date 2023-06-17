3104380980fc4be26c8470a92e6c7081fb6d31d8
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-17 22:11:48 +0200

    feat: implement copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,40 @@
+import fs from "fs";
+import path from "path";
+
 const copy = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = path.dirname(__filename);
+  const sourceFolder = path.join(__dirname, "files");
+  const targetFolder = path.join(__dirname, "files_copy");
+  const fsFailedErrorMessage = "FS operation failed";
+
+  try {
+    try {
+      await fs.promises.access(sourceFolder, fs.constants.F_OK);
+    } catch (error) {
+      throw new Error(fsFailedErrorMessage);
+    }
+
+    try {
+      await fs.promises.access(targetFolder, fs.constants.F_OK);
+      throw new Error(fsFailedErrorMessage);
+    } catch (error) {
+      if (error.code !== "ENOENT") {
+        console.error(error);
+      }
+    }
+
+    await fs.promises.mkdir(targetFolder);
+
+    const files = await fs.promises.readdir(sourceFolder);
+    for (const file of files) {
+      const sourceFile = path.join(sourceFolder, file);
+      const destinationFile = path.join(targetFolder, file);
+      await fs.promises.copyFile(sourceFile, destinationFile);
+    }
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await copy();

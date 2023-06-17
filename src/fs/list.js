be336544fe3667f1da1ab7c11852988d2ad70886
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-17 23:38:12 +0200

    feat: implement list function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import { readdir } from "fs/promises";
+import { dirname, join } from "path";
+
 const list = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const filesPath = join(__dirname, "files");
+  const fsFailedErrorMessage = "FS operation failed";
+
+  try {
+    await readdir(filesPath).then(files => console.log(files));
+  } catch (error) {
+    throw new Error(fsFailedErrorMessage);
+  }
 };
 
 await list();

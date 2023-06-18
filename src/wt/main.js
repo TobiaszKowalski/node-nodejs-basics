ab5f224bc494b5d632bf6857127b481d7f4b2561
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 16:48:03 +0200

    feat: implement sendResults and performCalculations functions

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from "worker_threads";
+import { dirname, join } from "path";
+import { cpus } from "os";
+
 const performCalculations = async () => {
-    // Write your code here
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const pathToWorker = join(__dirname, "worker.js");
+  const cpuCount = cpus().length;
+  const results = [];
+
+  await Promise.all(
+    Array.from({ length: cpuCount }, async (_, i) => {
+      const numberToSend = 10 + i;
+      try {
+        const worker = new Worker(pathToWorker);
+        worker.postMessage(numberToSend);
+        const computingResultMessage = await new Promise((resolve) =>
+          worker.on("message", resolve)
+        );
+        results[i] = {
+          status: "resolved",
+          data: computingResultMessage,
+        };
+        worker.terminate();
+      } catch (error) {
+        results[i] = {
+          status: "error",
+          data: null,
+        };
+      }
+    })
+  );
+  console.log(results);
 };
 
 await performCalculations();

b78517601d2ba42ea88856c894f0a055d0fce782
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 20:54:49 +0200

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,22 @@
+import { spawn } from "child_process";
+import { dirname, join } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const __filename = new URL(import.meta.url).pathname;
+    const __dirname = dirname(__filename);
+    const pathToScript = join(__dirname, "files", "script.js");
+
+    return new Promise((resolve, reject) => {
+        const childProcess = spawn("node", [pathToScript, ...args], {
+            stdio: [ "pipe", "pipe", "inherit"]
+        })
+        process.stdin.pipe(childProcess.stdin);
+        childProcess.stdout.pipe(process.stdout);
+
+        childProcess.on("close", code => code === 0 ? resolve() : reject());
+        childProcess.on("error", reject);
+    })
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(/* [someArgument1, someArgument2, ...] */);

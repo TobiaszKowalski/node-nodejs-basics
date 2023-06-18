2f8997460e6c057e9eed25c81a175a81e0b57968
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 12:25:18 +0200

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import {dirname, join} from "path";
+import crypto from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const hash = crypto.createHash('sha256');
+  const stream = fs.createReadStream(filePath);
+
+  for await (const chunk of stream) {
+    hash.update(chunk);
+  }
+
+  const hexHash = hash.digest('hex');
+  console.log(hexHash);
 };
 
 await calculateHash();

8e696b0713fb47376be1c981a92b8e7ac384c672
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 13:13:35 +0200

    feat: implement read function (Stream API)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,21 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+
+  const stream = createReadStream(filePath, "utf-8");
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  await new Promise((resolve, reject) => {
+    stream.on("end", resolve);
+    stream.on("error", reject);
+  });
 };
 
 await read();

e87d964d45b6aedaff3a7368868f12a5ff364df4
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 14:11:20 +0200

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,20 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const transformStream = new Transform({
+    transform(chunk, encoding, callback) {
+      const reversedText = chunk.toString().split("").reverse().join("");
+      this.push(reversedText);
+      callback();
+    },
+  });
+
+  await new Promise((resolve, reject) => {
+    process.stdin.pipe(transformStream).pipe(process.stdout);
+    process.stdin.on("end", resolve);
+    process.stdin.on("error", reject);
+    transformStream.on("error", reject);
+  });
 };
 
 await transform();

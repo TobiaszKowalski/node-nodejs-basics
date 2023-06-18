000765dcea15a7300305e569e27c7ae150c3d5ef
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 14:45:28 +0200

    feat: implement decompress function

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import { createReadStream, createWriteStream } from "fs";
+import { dirname, join } from "path";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fileToCompress.txt");
+  const zipFilePath = join(__dirname, "files", "archive.gz");
+  const readStream = createReadStream(zipFilePath);
+  const writeStream = createWriteStream(filePath);
+  const gunzip = createGunzip();
+
+  await new Promise((resolve, reject) => {
+    readStream
+      .pipe(gunzip)
+      .pipe(writeStream)
+      .on("finish", resolve)
+      .on("error", reject);
+  });
 };
 
 await decompress();

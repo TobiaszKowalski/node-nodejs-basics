446ddeabef083ab55b2698b35117cb8e6befca61
Aliaksandr <aliaksandr.chyrtsov@sap.com>
2023-06-18 13:32:21 +0200

    feat: implement write function (Stream API)

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,18 @@
+import { createWriteStream } from "fs";
+import { dirname, join, resolve } from "path";
+
 const write = async () => {
-    // Write your code here 
+  const __filename = new URL(import.meta.url).pathname;
+  const __dirname = dirname(__filename);
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+
+  const writeStream = createWriteStream(filePath, "utf-8");
+  process.stdin.pipe(writeStream);
+
+  await new Promise((resolve, reject) => {
+    writeStream.on("finish", resolve);
+    writeStream.on("error", reject);
+  });
 };
 
 await write();
